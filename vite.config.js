9526b65aba301f59159cec022ff7294640a7ec20
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-16 20:34:10 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    open: true,
+  },
+  build: {
+    lib: {
+      entry: "src/my-element.js",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
+});
