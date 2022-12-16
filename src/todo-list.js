e38179947cc23d26d5da03659c8ee2acd81d54cf
Piotr Filipiak <kiddhkane@gmail.com>
2022-12-16 23:08:03 +0100

    Working on the task list.

--- src/todo-list.js
+++ src/todo-list.js
@@ -3,19 +3,37 @@ import { LitElement, html } from "lit";
 export class TodoList extends LitElement {
   static get properties() {
     return {
-      message: {
-        text: { type: String },
-      },
+      _tasks: { state: true },
+      hideCompleted: {},
     };
   }
 
   constructor() {
     super();
-    this.message = "Hello Task";
+    this._tasks = [{ text: "Write a task list", completed: false }];
+    this.hideCompleted = false;
+  }
+
+  addTask() {
+    alert("Added");
+  }
+
+  removeTask() {
+    alert("Removed");
   }
 
   render() {
-    return html`<p>${this.message}</p>`;
+    const items = 0;
+
+    return html`
+      <div>
+        <h1>Tasks:</h1>
+        <ul></ul>
+        <input type="text"></input>
+        <button @click=${this.addTask}>Add a task</button>
+        <input type="checkbox"></input>
+      </div>
+    `;
   }
 }
 
